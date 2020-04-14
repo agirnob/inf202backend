36f2e464231e65cca991c1e43a194d23e9ab7281
agirnob <kakcayigitbaran@gmail.com>
2020-04-14 15:59:05 +0300

    fxml dosyasının açılması sağlandı.Uyarılar düzeltildi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    public static Stage window;
     public static void main(String[] args) throws Exception {
         Class.forName("org.h2.Driver");
         Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "", "");
         System.out.println("bağlanıldı");
-        conn.close();
-        conn = DriverManager.getConnection("" + "jdbc:h2:~/test", "", "");
-        System.out.println("tekrar bağlanıldı");
         conn.close();
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Hello World!");
-        Pane myPane = FXMLLoader.load(getClass().getResource("home.fxml"));
-        Scene myScene = new Scene(myPane);
-        stage.setScene(myScene);
+        window=stage;
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setTitle("deneme");
+        stage.setScene(scene);
         stage.show();
     }
 }
--- src/com/company/controllerHome.java
+++ src/com/company/controllerHome.java
@@ -1,4 +1,38 @@
 package com.company;
 
-public class controllerHome {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class controllerHome extends Main implements Initializable {
+
+
+    public void homePage(ActionEvent actionEvent) {
+
+    }
+
+    public void kisiEkleDegistir(ActionEvent actionEvent) throws IOException {
+       Parent root= FXMLLoader.load(getClass().getResource("kisiEkleDegistir.fxml"));
+       Scene scene = new Scene(root);
+       window.setScene(scene);
+
+    }
+
+    public void renkDegistir(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
