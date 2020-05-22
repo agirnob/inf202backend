6fb837445a22c9f4751b3390cf589a4c4f7a7260
agirnob <kakcayigitbaran@gmail.com>
2020-05-22 19:57:18 +0300

    addiered mehr stadten

--- src/agirnob/ilceler.java
+++ src/agirnob/ilceler.java
@@ -19,31 +19,93 @@ public class ilceler {
     private final String[] Ankara ={"Akyurt","Altındağ","Ayaş","Bala","Beypazarı","Çamlıdere","Çankaya","Çubuk","Elmadağ","Etimefgut",
             "Evren","Gölbaşı","Güdül","Haymana","Kalecik","Kahramankazan","Keçiören","Kızılcahamam","Mamak","Nallıhan","Polatlı","Pursakar","Sincan","Şereflikoçhisar","Yenimahalle"};
 
-    private final String[] aksaray =
+    private final String[] antalya ={};
 
-    private final String[] aksaray =
+    private final String[] ardahan ={};
 
-    private final String[] aksaray =
+    private final String[] artvin ={};
 
-    private final String[] aksaray =
+    private final String[] Aydın ={};
 
-    private final String[] aksaray =
+    private final String[] Balıkesir ={};
 
-    private final String[] aksaray =
+    private final String[] Bartın ={};
 
-    private final String[] aksaray =
+    private final String[] Batman ={};
 
-    private final String[] aksaray =
+    private final String[] Bayburt ={};
 
-    private final String[] aksaray =
+    private final String[] Bilecik ={};
 
-    private final String[] aksaray =
+    private final String[] Bingöl ={};
 
-    private final String[] aksaray =
+    private final String[] Bitlis ={};
 
-    private final String[] aksaray =
+    private final String[] Bolu ={};
 
-    private final String[] aksaray =
+    private final String[] Burdur ={};
+    private final String[] Bursa ={};
+    private final String[] Çanakkal ={};
+    private final String[] Çankırı ={};
+    private final String[] Çorum ={};
+    private final String[] Denizli ={};
+    private final String[] Diyarbakır ={};
+    private final String[] Düzce ={};
+    private final String[] Edirne ={};
+    private final String[] Elazığ ={};
+    private final String[] Erzincan ={};
+    private final String[] Erzurum ={};
+    private final String[] Eskişehir ={};
+    private final String[] Gaziantep ={};
+    private final String[] Giresun ={};
+    private final String[] Gümüşhane ={};
+    private final String[] Hakkâri ={};
+    private final String[] Hatay ={};
+    private final String[] Iğdır ={};
+    private final String[] Isparta ={};
+    private final String[] İstanbul ={};
+    private final String[] İzmir ={};
+    private final String[] Kahramanmaraş ={};
+    private final String[] Karabük ={};
+    private final String[] Karaman ={};
+    private final String[] Kars ={};
+    private final String[] Kastamonu ={};
+    private final String[] Kayseri ={};
+    private final String[] Kırıkkale ={};
+    private final String[] Kırklareli ={};
+    private final String[] Kırşehir ={};
+    private final String[] Kilis ={};
+    private final String[] Kocaeli ={};
+    private final String[] Konya ={};
+    private final String[] Kütahya ={};
+    private final String[] Malatya ={};
+    private final String[] Manisa ={};
+    private final String[] Mardin ={};
+    private final String[] Mersin ={};
+    private final String[] Muğla ={};
+    private final String[] Muş ={};
+    private final String[] Nevşehir ={};
+    private final String[] Niğde ={};
+    private final String[] Ordu ={};
+    private final String[] Osmaniye ={};
+    private final String[] Rize ={};
+    private final String[] Sakarya ={};
+    private final String[] Samsun ={};
+    private final String[] Siirt ={};
+    private final String[] Sinop ={};
+    private final String[] Sivas ={};
+    private final String[] Şanlıurfa ={};
+    private final String[] Şırnak ={};
+    private final String[] Tekirdağ ={};
+    private final String[] Tokat ={};
+    private final String[] Trabzon ={};
+    private final String[] Tunceli ={};
+    private final String[] Uşak ={};
+    private final String[] Van ={};
+    private final String[] Yalova ={};
+    private final String[] Yozgat ={};
+    private final String[] Zonguldak ={};
+
 
 
 
